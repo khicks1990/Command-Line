f27edd62e0cf6e0502fd26131a8dfc301af0f652
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-19 13:06:05 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+   This program displays the arguments passed to
+   it from the operating system command line.
+*/
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      for (int index = 0; index < args.length; index++)
+         System.out.println(args[index]);
+   }
+}
+
+
+//java Main How does this work?
